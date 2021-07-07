313dfd786d4c977803bd4151e706dcd78294211e
NguyenPhatThanhTai <57514749+NguyenPhatThanhTai@users.noreply.github.com>
2021-07-07 19:23:10 +0700

    Update #3
    SignalR

---
+++ Controllers/severHub.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Choigido.Controllers
+{
+    public class severHub : Hub
+    {
+        //public void Send(string name, string message)
+        //{
+        //    // Call the addNewMessageToPage method to update clients.
+        //    Clients.All.addNewMessageToPage(name, message);
+        //}
+        public async Task JoinRoom(string roomName, string name)
+        {
+            await Groups.Add(Context.ConnectionId, roomName);
+            Clients.Group(roomName).addChatMessage(Context.User.Identity.Name + name + " joined.");
+        }
+
+        public async Task LeaveRoom(string roomName, string name)
+        {
+            await Groups.Remove(Context.ConnectionId, roomName);
+            Clients.Group(roomName).addChatMessage(Context.User.Identity.Name + name + " leave.");
+        }
+        public async Task Send(string group, string message)
+        {
+            // Call the addMessage method on all clients            
+            Clients.Group(group).addChatMessage(Context.User.Identity.Name + "Group Message " + message);
+        }
+    }
+}
